3cc9a98bd6412ddfed6e95c7bcfe1765842da2ea
Sze Wong Liaw <liawsw@live.com>
2023-03-24 21:17:28 +0800

    Update Form Name

---
+++ SQLite/SQLite.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace SQLite
+{
+    partial class SQLite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDelDB = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(453, 17);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(50, 26);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(306, 21);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(129, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name";
+            // 
+            // btnDelDB
+            // 
+            this.btnDelDB.Location = new System.Drawing.Point(437, 252);
+            this.btnDelDB.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelDB.Name = "btnDelDB";
+            this.btnDelDB.Size = new System.Drawing.Size(89, 31);
+            this.btnDelDB.TabIndex = 6;
+            this.btnDelDB.Text = "Delete Table";
+            this.btnDelDB.UseVisualStyleBackColor = true;
+            this.btnDelDB.Click += new System.EventHandler(this.btnDelDB_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, -2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(242, 304);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // SQLite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 303);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelDB);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "SQLite";
+            this.Text = "SQLite";
+            this.Load += new System.EventHandler(this.SQLite_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDelDB;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ SQLite/SQLite.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//https://www.dotnetcurry.com/aspnet/143/convert-data-reader-to-data-table
+
+namespace SQLite
+{
+    public partial class SQLite : Form
+    {
+        public SQLite()
+        {
+            InitializeComponent();
+        }
+
+        private void SQLite_Load(object sender, EventArgs e)
+        {
+            using (var connection = new SQLiteConnection("Data Source=user.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.Parameters.Clear();
+
+                command.CommandText =
+                @"  CREATE TABLE IF NOT EXISTS user (
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL
+                    );
+                ";
+                command.ExecuteNonQuery();
+
+                command.CommandText =
+                @"
+                       SELECT COUNT(*) 
+                       FROM user;
+                ";
+                var num = command.ExecuteScalar();
+                //@"SELECT last_insert_rowid()"
+
+                if (Convert.ToInt32(num) == 0)
+                {
+                    command.CommandText =
+                        @"  INSERT INTO user(name)
+                            VALUES('Admin'),
+                                  ('Alex'),
+                                  ('Chein' );
+                     ";
+                    command.ExecuteNonQuery();
+                }
+
+                command.CommandText =
+                    @"  SELECT *
+                        FROM user;
+                ";
+                SQLiteDataReader reader = command.ExecuteReader();
+                DataTable dt = new DataTable("Users");
+                dt.Columns.Add("ID", typeof(int));
+                dt.Columns.Add("Name", typeof(string));
+                dt.Load(reader);
+                dataGridView1.DataSource = dt;
+
+                connection.Close();
+            }
+        }
+
+        private void btnDelDB_Click(object sender, EventArgs e)
+        {
+            using (var connection = new SQLiteConnection("Data Source=user.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.Parameters.Clear();
+                command.CommandText =
+                    @"  DROP TABLE IF EXISTS user;
+                ";
+                command.ExecuteNonQuery();
+
+                dataGridView1.DataSource = null;
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (var connection = new SQLiteConnection("Data Source=user.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.Parameters.Clear();
+                command.CommandText =
+                @"  INSERT INTO user (name)
+                    VALUES ($name);
+
+                    SELECT *
+                    FROM user;
+                ";
+                command.Parameters.AddWithValue("$name", txtName.Text);
+                //command.ExecuteNonQuery();
+                SQLiteDataReader reader = command.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dataGridView1.DataSource = dt;
+            }
+        }
+    }
+}
